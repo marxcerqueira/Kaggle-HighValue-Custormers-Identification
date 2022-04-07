109e1c6937c953eaf1bc74093a21af0389cff80f
marxcerqueira <marxv49@gmail.com>
2022-04-06 23:43:30 -0300

    deploy

---
+++ run_model_production.sh
@@ -0,0 +1,11 @@
+# variables
+cdt=$(date +'%Y-%m-%dT%H:%M:%S')
+
+# path
+path='/home/ubuntu/Kaggle-HighValue-Custormers-Identification'
+path_to_papermill='/home/ubuntu/.pyenv/versions/HighValue-Customers-Identification/bin'
+
+# papermill command
+$path_to_papermill/papermill $path/src/models/c9.0-mc-deploy.ipynb $path/reports/c9.0-mc-deploy-$cdt.ipynb
+
+

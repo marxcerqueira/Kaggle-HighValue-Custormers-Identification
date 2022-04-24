545d2e8375b751a30d314f20b312960c037723f3
marxcerqueira <marxv49@gmail.com>
2022-04-24 14:18:37 -0300

    fixing bugs

--- run_model.sh
+++ run_model.sh
@@ -6,7 +6,7 @@ path='/home/marxcerqueira/repos/Kaggle-HighValue-Custormers-Identification'
 path_to_papermill='/home/marxcerqueira/.local/bin/'
 
 # papermill command
-$path_to_papermill/papermill $path/src/models/c9.0-mc-deploy.ipynb $path/reports/c9.0-mc-deploy-$cdt.ipynb
+$path_to_papermill/papermill $path/src/models/c9.1-mc-deploy.ipynb $path/reports/c9.1-mc-deploy-$cdt.ipynb
 
 
 
